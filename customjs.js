4a0da7beda6f99522c3ed0c1331f0901198971a7
OluwaKemmy Mary Omoshoro <kemmyomosh@gmail.com>
2022-07-01 05:50:28 +0200

    commit fixing buttons

--- customjs.js
+++ customjs.js
@@ -1,16 +1,27 @@
 
 function greetingMsg(){
-     hideimg(); // hide at beginning
 
-     $('#button-greet').on('click', function(){
-           // show button on click
-       $('#img-gif').css("display", "block");
-     });
-     
+     $(document).ready(function () {
+          hidegreetingMsg();
+
+          $('#button-greet').click(function () {
+              $('#img-gif').show();
+              $('#button-hide-greet').attr('disabled', false);
+              $('#button-greet').attr('disabled', true);
+          });
+      }); 
 
 }//end_function
 
-function hideimg() {
-     $('#img-gif').css("display", "none");
-}
+function hidegreetingMsg(){
+     $('#img-gif').hide();
+     $('#button-hide-greet').attr('disabled', true);
+     $('#button-hide-greet').click(function () {
+         $('#img-gif').hide();
+         $('#button-hide-greet').attr('disabled', true);
+         $('#button-greet').attr('disabled', false);
+
+     });
+
+}//end_function
 
