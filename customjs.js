7dc8d088ab53474801dcfad8f1fcbe549711ce0e
OluwaKemmy Mary Omoshoro <kemmyomosh@gmail.com>
2022-07-02 02:21:12 +0200

    commit changed features ('on master')

--- customjs.js
+++ customjs.js
@@ -1,27 +1,37 @@
 
 function greetingMsg(){
-
+    
      $(document).ready(function () {
-          hidegreetingMsg();
+          $('#img-gif').hide();
+          $('#button-hide-greet').attr('disabled', true);
+           $('#button-hide-greet').click(function () {
+           $('#img-gif').hide();
+           $('#button-hide-greet').attr('disabled', true);
+           $('#button-greet').attr('disabled', false);
+
+      });
 
           $('#button-greet').click(function () {
               $('#img-gif').show();
               $('#button-hide-greet').attr('disabled', false);
               $('#button-greet').attr('disabled', true);
           });
-      }); 
+     }); 
 
 }//end_function
 
+/*
 function hidegreetingMsg(){
-     $('#img-gif').hide();
-     $('#button-hide-greet').attr('disabled', true);
-     $('#button-hide-greet').click(function () {
+   $(document).ready(function () {
          $('#img-gif').hide();
-         $('#button-hide-greet').attr('disabled', true);
-         $('#button-greet').attr('disabled', false);
+              $('#button-hide-greet').attr('disabled', true);
+               $('#button-hide-greet').click(function () {
+               $('#img-gif').hide();
+               $('#button-hide-greet').attr('disabled', true);
+               $('#button-greet').attr('disabled', false);
 
+          });
      });
 
 }//end_function
-
+*/
